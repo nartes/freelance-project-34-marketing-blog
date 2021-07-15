6c9227128955fa69fd229141ecdb88e60e2bc3cf
Siarhei Siniak <siarheisiniak@yahoo.com>
2021-07-15 07:57:36 +0300

    [~] Refactor

--- python/tasks/jigsaw_toxic.py
+++ python/tasks/jigsaw_toxic.py
@@ -1,6 +1,10 @@
+import pprint
+import requests
+
+
 def kernel_1_sample_scrap():
     with requests.get(
         'https://dev.to/kunaal438/media-query-everything-you-need-for-responsive-design-b8g',
     ) as p:
-        t1 = p.content
+        t1 = p.content.decode('utf-8')
     pprint.pprint(t1)
