d108e75b76948468ecd94cae4ed1b24b985f2674
Siarhei Siniak <siarheisiniak@yahoo.com>
2021-07-15 07:59:54 +0300

    [~] Refactor

--- python/tasks/jigsaw_toxic.py
+++ python/tasks/jigsaw_toxic.py
@@ -7,4 +7,6 @@ def kernel_1_sample_scrap():
         'https://dev.to/kunaal438/media-query-everything-you-need-for-responsive-design-b8g',
     ) as p:
         t1 = p.content.decode('utf-8')
-    pprint.pprint(t1)
+    t2 = pyquery.PyQuery(t1)
+    t3 = t2('.comment_content')
+    pprint.pprint(t3)
