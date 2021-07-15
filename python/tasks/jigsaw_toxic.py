50ca593a1c29fd1e000fa11110f8768f7af1b854
Siarhei Siniak <siarheisiniak@yahoo.com>
2021-07-15 09:59:41 +0300

    [~] Refactor

--- python/tasks/jigsaw_toxic.py
+++ python/tasks/jigsaw_toxic.py
@@ -242,13 +242,14 @@ def kernel_3(
 
     # %% [code]
     def roc_auc(predictions,target):
+        import sklearn.metrics
         '''
         This methods returns the AUC Score when given the Predictions
         and Labels
         '''
 
-        fpr, tpr, thresholds = metrics.roc_curve(target, predictions)
-        roc_auc = metrics.auc(fpr, tpr)
+        fpr, tpr, thresholds = sklearn.metrics.roc_curve(target, predictions)
+        roc_auc = sklearn.metrics.auc(fpr, tpr)
         return roc_auc
 
     # %% [code]
