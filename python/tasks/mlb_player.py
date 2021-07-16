8ef31c33a23b3417ad9e69c698d0b4190cc3a32e
Siarhei Siniak <siarheisiniak@yahoo.com>
2021-07-16 10:29:56 +0300

    [~] Refactor

--- python/tasks/mlb_player.py
+++ python/tasks/mlb_player.py
@@ -1,22 +1,34 @@
 import glob
 import os
 import pandas
+import pickle
 
 def kernel_1():
-    t1 = '/kaggle/input/mlb-player-digital-engagement-forecasting'
-    t2 = glob.glob(
-        os.path.join(
-            t1,
-            '*.csv'
+    t4 = 'kernel_1-t3.dat'
+
+    def preprocess(t4):
+        t1 = '/kaggle/input/mlb-player-digital-engagement-forecasting'
+        t2 = glob.glob(
+            os.path.join(
+                t1,
+                '*.csv'
+            )
         )
-    )
-    t3 = {
-        o : pandas.read_csv(o)
-        for o in t2
-    }
+
+        t3 = {
+            o : pandas.read_csv(o)
+            for o in t2
+        }
+
+        with io.open(t4, 'wb') as f:
+            pickle.dump(t3, f)
+
+    if not os.path.exists(t4):
+        preprocess(t4=t4)
+
+    with io.open(t4, 'rb') as f:
+        t3 = pickle.load(f)
 
     return dict(
-        t1=t1,
-        t2=t2,
         t3=t3,
     )
