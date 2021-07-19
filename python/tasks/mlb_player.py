b2d8b6cfc0aaf27ebdde262f5535f38af6dfd90a
Siarhei Siniak <siarheisiniak@yahoo.com>
2021-07-19 10:13:50 +0300

    [~] Refactor

--- python/tasks/mlb_player.py
+++ python/tasks/mlb_player.py
@@ -195,7 +195,7 @@ def kernel_4(
         }
     """
 
-    t1 = numpy.where(o_3['t5']['events']['gamePk'] == 634280)
+    t1 = numpy.where(o_3['t5']['events']['gamePk'] == 634280)[0]
     t2 = o_3['t5']['events'].sel(index=t1)
     t3 = o_3['t5']['games'].to_dataframe().iloc[-1].to_dict()
     assert t3['gamePk'] == 634280
