64fdde31e4cde3460c7f6a5393fe3f25931eb2ee
Siarhei Siniak <siarheisiniak@gmail.com>
2022-02-01 09:45:07 +0300

    [~] Refactor

--- d1/f1.sh
+++ d1/f1.sh
@@ -3,3 +3,4 @@
 cp dotfiles/.local/bin/commands ~/.local/bin/commands
 mkdir -p ~/.sway
 cp dotfiles/.sway/config ~/.sway/config
+cp dotfiles/.zshenv ~/.zshenv
--- dotfiles/.zshenv
+++ dotfiles/.zshenv
@@ -1,2 +1,5 @@
 export EDITOR=vim
 export PATH=~/.local/bin:$PATH
+export GTK_IM_MODULE=ibus
+export XMODIFIERS=@im=ibus
+export QT_IM_MODULE=ibus
