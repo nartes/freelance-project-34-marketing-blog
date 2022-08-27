17c7a7b898e537181da0da872c9a8ce94a45fc05
Siarhei Siniak <siarheisiniak@gmail.com>
2022-08-27 13:45:29 +0300

    Revert "[~] Update fast prompt ipython fix"
    This reverts commit b7601367e49b8ed77fc719eb0c0747823b279a4c.

--- dotfiles/.ipython/profile_default/ipython_config.py
+++ dotfiles/.ipython/profile_default/ipython_config.py
@@ -5,4 +5,3 @@ c.InteractiveShell.history_load_length = 100 * 1000 * 1000
 c.InteractiveShell.pdb = True
 c.TerminalInteractiveShell.editing_mode = 'vi'
 c.TerminalInteractiveShell.modal_cursor = False
-c.TerminalInteractiveShell.fast_prompt = True
