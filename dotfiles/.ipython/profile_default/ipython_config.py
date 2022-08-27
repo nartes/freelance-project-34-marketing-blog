65a094b86608f1173548a2198b9f8cf66c28bb9e
Siarhei Siniak <siarheisiniak@gmail.com>
2022-08-27 13:46:30 +0300

    [~] Refactor

--- dotfiles/.ipython/profile_default/ipython_config.py
+++ dotfiles/.ipython/profile_default/ipython_config.py
@@ -5,3 +5,4 @@ c.InteractiveShell.history_load_length = 100 * 1000 * 1000
 c.InteractiveShell.pdb = True
 c.TerminalInteractiveShell.editing_mode = 'vi'
 c.TerminalInteractiveShell.modal_cursor = False
+c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
