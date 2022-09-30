c0636f8f4646b4161469981a10187e7260b896ab
Siarhei Siniak <siarheisiniak@gmail.com>
2022-09-30 13:18:34 +0300

    [~] Refactor

--- dotfiles/.ipython/profile_default/ipython_config.py
+++ dotfiles/.ipython/profile_default/ipython_config.py
@@ -1,4 +1,18 @@
-c.InteractiveShellApp.exec_lines = ['%autoreload 2',]
+c.InteractiveShellApp.exec_lines = [
+    '%autoreload 2',
+    r'''
+def ipython_update_shortcuts():
+    import IPython
+    t1 = IPython.get_ipython()
+    t2 = t1.pt_app
+    t3 = [o for o in t2.key_bindings.bindings if 'f2' in repr(o).lower()]
+    assert len(t3) == 1
+    t4 = t3[0]
+    t2.key_bindings.remove(t4.handler)
+    t2.key_bindings.add('i')(t4.handler)
+    ''',
+    'ipython_update_shortcuts()',
+]
 c.InteractiveShellApp.extensions = ['autoreload']
 c.InteractiveShell.history_length = 100 * 1000 * 1000
 c.InteractiveShell.history_load_length = 100 * 1000 * 1000
