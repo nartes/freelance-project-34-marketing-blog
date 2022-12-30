cb315f452ce87fa523180c36ac66da8524c0bed1
Siarhei Siniak <siarheisiniak@gmail.com>
2022-12-30 22:49:03 +0300

    [~] Refactor

--- d1/f1.sh
+++ d1/f1.sh
@@ -5,6 +5,7 @@ cp dotfiles/.local/bin/commands ~/.local/bin/commands
 mkdir -p ~/.sway
 cp dotfiles/.sway/config ~/.sway/config
 cp dotfiles/.zshenv ~/.zshenv
+cp dotfiles/.zshrc ~/.zshrc
 cp dotfiles/.vimrc ~/.vimrc
 cp dotfiles/.tmux.conf ~/.tmux.conf
 cp -rp \
