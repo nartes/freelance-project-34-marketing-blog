2fb6f3893f090146a38556f70b7f3d0da6072dff
Siarhei Siniak <siarheisiniak@gmail.com>
2024-04-13 13:11:11 +0300

    [~] Refactor

--- d1/f1.sh
+++ d1/f1.sh
@@ -8,7 +8,8 @@ cp dotfiles/.zshenv ~/.zshenv
 cp dotfiles/.zshrc ~/.zshrc
 cp dotfiles/.vimrc ~/.vimrc
 cp dotfiles/.py3.vimrc ~/.py3.vimrc
-cp dotfiles/.tmux.conf ~/.tmux.conf
+cp dotfiles/.py3.vimrc ~/.py3.vimrc
+cp dotfiles/.gitconfig ~/.gitconfig
 cp -rp \
     dotfiles/.ipython/profile_default/ipython_config.py \
     ~/.ipython/profile_default/ipython_config.py
